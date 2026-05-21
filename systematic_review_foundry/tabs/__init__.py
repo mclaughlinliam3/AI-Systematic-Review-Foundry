887a5272436ca758fb312ed20a4dbbdae7159f1c
mclaughlinliam3 <161753863+mclaughlinliam3@users.noreply.github.com>
2026-05-21 09:50:55 -0500

    Delete systematic_review_foundry directory

--- systematic_review_foundry/tabs/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Tabs package for Systematic Review Foundry.
-"""
-from tabs.main_tab import MainReviewTab
-from tabs.sources_tab import SourcesTab
-from tabs.topics_stats_tab import TopicsTab, StatsTab
-
-__all__ = ['MainReviewTab', 'SourcesTab', 'TopicsTab', 'StatsTab']
